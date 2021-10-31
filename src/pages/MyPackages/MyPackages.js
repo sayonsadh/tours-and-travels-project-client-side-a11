8dc50471eb3ac808f9332070d608cd1105dfb52d
sayonsadh <sadhkhan1999@gmail.com>
2021-10-31 09:31:34 -0700

    fourth commit

--- src/pages/MyPackages/MyPackages.js
+++ src/pages/MyPackages/MyPackages.js
@@ -1,33 +1,39 @@
 import React, { useEffect, useState } from 'react';
+import useAuth from '../../hooks/useAuth';
 import './MyPackages.css';
 
 const MyPackages = () => {
-
+    const { user } = useAuth();
     const [tours, setTours] = useState([]);
 
-    useEffect( () => {
+    useEffect(() => {
         fetch('https://frightful-zombie-62130.herokuapp.com/tours')
-        .then(res => res.json())
-        .then(data => setTours(data))
+            .then(res => res.json())
+            .then(data => {
+                const selectPackage = data.filter(e => 
+                    e.email === user.email,
+                )
+                setTours(selectPackage);
+            })
     }, [])
-    
-    return ( 
+
+    return (
         <div>
             <h5 className="fst-italic mt-5 p-3">Here Is Your selected Package</h5>
             <div className="myPackage-container m-5">
-            {
-                tours.map(tour =>
-                    <div className="border border-info w-100 bg-info bg-opacity-25 rounded-3 " key={tour._id}>
-                        <h2>Congratulation</h2>
-                        <h5>Name:- {tour.name}</h5>
-                        <h5>Email:- {tour.email}</h5>
-                        <h6>Phone:- {tour.phone}</h6>
-                        <h6>Address:- {tour.address}</h6>
-                        <h6>Destination:- {tour.destination}</h6>
-                        <h6>Travel date:- {tour.date}</h6>
-                    </div>
-                     )
-            }
+                {
+                    tours.map(tour =>
+                        <div className="border border-info w-100 bg-info bg-opacity-25 rounded-3 " key={tour._id}>
+                            <h2>Congratulation</h2>
+                            <h5>Name:- {tour.name}</h5>
+                            <h5>Email:- {tour.email}</h5>
+                            <h6>Phone:- {tour.phone}</h6>
+                            <h6>Address:- {tour.address}</h6>
+                            <h6>Destination:- {tour.destination}</h6>
+                            <h6>Travel date:- {tour.date}</h6>
+                        </div>
+                    )
+                }
             </div>
         </div>
     );
